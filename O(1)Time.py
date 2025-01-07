6f14b837e39f02979b94b2fb90c7d63554577b3e
Siddanth-lab <siddanthgarlapati@gmail.com>
2025-01-07 18:39:00 +0100

    Add files via upload

---
+++ O(1)Time.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by the user is", n)
+    iteration+=1
+    print("Total iterations done by the code is ",iteration,"\n")
+
+printnumber(10)
+printnumber(5)
+print("\n With any 'n' the time taken by our code won't change")
---
+++ O(n)Time.py
@@ -0,0 +1,10 @@
+def OnTime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("When n is",n,"Iterations =",iteration)
+OnTime(948302)
+OnTime(97653)
+OnTime(6754)
+
+print("\nWith every 'n' the time taken and iterations will increase")
